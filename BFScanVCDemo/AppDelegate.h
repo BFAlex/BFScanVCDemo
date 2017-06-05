6b4f27be7119b369669862b8fa260364ca139bbe
BFAlex <838318741@qq.com>
2017-06-05 16:59:25 +0800

    Initial Commit

---
+++ BFScanVCDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BFScanVCDemo
+//
+//  Created by Readboy_BFAlex on 2017/6/5.
+//  Copyright © 2017年 Readboy_BFAlex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BFScanVCDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BFScanVCDemo
+//
+//  Created by Readboy_BFAlex on 2017/6/5.
+//  Copyright © 2017年 Readboy_BFAlex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
